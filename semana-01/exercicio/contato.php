2582cd93602efcfa3c28f20917948170dc8fe47b
Carol <carol@pop-os.localdomain>
2020-06-19 12:53:09 -0300

    php

--- semana-01/exercicio/contato.php
+++ semana-01/exercicio/contato.php
@@ -1,3 +1,46 @@
 <?php
 
+$valido = false;
+
 $nome = filter_input(INPUT_POST, 'txtNome', FILTER_SANITIZE_STRING);
+if(empty($nome)){
+    "Campo nome obrigatório";
+}
+
+$email = filter_input(INPUT_POST, 'txtEmail', FILTER_SANITIZE_STRING);
+
+$telefone = filter_input(INPUT_POST, 'txtFone', FILTER_SANITIZE_STRING);
+
+$assunto = filter_input(INPUT_POST, 'txtAssunto', FILTER_SANITIZE_STRING);
+
+$mensagem = filter_input(INPUT_POST, 'txtMessage', FILTER_SANITIZE_STRING);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contato</title>
+</head>
+
+<body>
+
+    <?php if ($valido) : ?>
+
+        <p><?=$nome?></p>
+        <p><?=$email?></p>
+        <p><?=$telefone?></p>
+        <p><?=$assunto?></p>
+        <p><?=$mensagem?></p>
+
+    <?php else : ?>
+
+        <p>Inválido</p>
+
+    <?php endif; ?>
+</body>
+
+</html>
